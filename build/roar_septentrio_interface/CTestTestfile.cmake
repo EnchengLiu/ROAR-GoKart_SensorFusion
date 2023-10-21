086828e731882e1b630c2253b6a427f942c0fadb
EnchengLiu <liuencheng@sjtu.edu.cn>
2023-10-21 15:13:22 -0700

    Finished Recording Data

---
+++ build/roar_septentrio_interface/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/openark/ros2_ws/src/roar_septentrio_interface
+# Build directory: /home/openark/ros2_ws/build/roar_septentrio_interface
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/build/roar_septentrio_interface/CMakeFiles/roar_septentrio_interface_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/roar_septentrio_interface_node.dir/src/roar_septentrio_interface_node.cpp.o"
+  "roar_septentrio_interface_node"
+  "roar_septentrio_interface_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/roar_septentrio_interface_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
