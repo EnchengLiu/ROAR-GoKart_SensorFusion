e89a2385fe91278009b9f0ea9d24806e3e796dec
EnchengLiu <liuencheng@sjtu.edu.cn>
2024-01-27 15:43:02 -0800

    Resolved merge conflicts

--- build/septentrio_gnss_driver/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/openark/ros2_ws/src/septentrio_gnss_driver
-# Build directory: /home/openark/ros2_ws/build/septentrio_gnss_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("septentrio_gnss_driver__py")
-subdirs("test")
